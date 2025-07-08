cc3fcc8dfdc0a61f69a1e9fb4691dfd4a48fb652
lig7t <7551374-lig7t@users.noreply.replit.com>
2025-07-08 07:59:30 +0000

    add more messages and make a random number generator function

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const messages = [];
+const messages = [
+  "Q: Did you hear about the actor who fell through the floorboards? A: He was just going through a stage",
+  "Q: What do you call fake spaghetti? A: An impasta",
+  "Q: How many tickles does it take to make an octopus laugh? A: Ten-tickles",
+  "Q: Why do we tell actors to break a leg? A: Because every play has a cast",
+  "Q: What do you call cheese that isn't yours? A: Nacho cheese",
+  "Q: Why is a baseball stadium always cold? A: Because it’s full of fans!",
+  "Q: How do you get a tissue to dance? A: You put a boogie in it",
+  "Q: Did you hear about the famous pickle? A: He was a big dill!",
+  "Q: What do you call a can opener that doesn't work? A: A can't opener",
+  "Q: What do you call a sad cup of coffee? A: Depresso",
+  "Q: Why did the picture go to jail? A: Because it was framed",
+  "Q: Why didn’t the teddy bear eat dessert? A: He was stuffed",
+  "Q: What do you call a bee that can’t make up its mind? A: A maybe",
+  "Q: What do you call a cow with no legs? A: Ground beef",
+  "Q: Why did the coffee file a police report? A: It got mugged",
+  "Q: What do you call a cow with two legs? A: Lean beef",
+  "Q: Why did the tomato turn red? A: Because it saw the salad dressing!",
+  "Q: Why did the coffee file a police report? A: It got mugged",
+  "Q: Why can’t you trust atoms? A: They make up everything",
+];
 
-function randNum () {'Q: Did you hear about the actor who fell through the floorboards? A: He was just going through a stage','Q: What do you call fake spaghetti? A: An impasta','Q: How many tickles does it take to make an octopus laugh? A: Ten-tickles','Q: Why do we tell actors to break a leg? A: Because every play has a cast','Q: What do you call cheese that isn\'t yours? A: Nacho cheese','Q: Why is a baseball stadium always cold? A: Because it’s full of fans!','Q: How do you get a tissue to dance? A: You put a boogie in it','Q: Did you hear about the famous pickle? A: He was a big dill!'};
+function randNum() {
+  return Math.floor(Math.random() * messages.length);
+}
 
-function messageOut (num) {};
+function messageOut(num) {}
 
 console.log(messageOut(num));
