5771cd0d0478850354e4fd4d16051d9db85f002a
lig7t <7551374-lig7t@users.noreply.replit.com>
2025-07-08 07:55:09 +0000

    add different messages to the message array

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const messages = [];
 
-function randNum () {}
-function messageOut () {}
+function randNum () {'Q: Did you hear about the actor who fell through the floorboards? A: He was just going through a stage','Q: What do you call fake spaghetti? A: An impasta','Q: How many tickles does it take to make an octopus laugh? A: Ten-tickles','Q: Why do we tell actors to break a leg? A: Because every play has a cast','Q: What do you call cheese that isn\'t yours? A: Nacho cheese','Q: Why is a baseball stadium always cold? A: Because it’s full of fans!','Q: How do you get a tissue to dance? A: You put a boogie in it','Q: Did you hear about the famous pickle? A: He was a big dill!'};
+
+function messageOut (num) {};
+
+console.log(messageOut(num));
